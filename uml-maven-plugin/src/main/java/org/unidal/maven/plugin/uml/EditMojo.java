3691be4b8a2da475764fda9a3fc3bae79345c846
Frankie Wu <qmwu2000@gmail.com>
2014-12-28 14:39:54 +0800

    uml-maven-plugin refactor

--- uml-maven-plugin/src/main/java/org/unidal/maven/plugin/uml/EditMojo.java
+++ uml-maven-plugin/src/main/java/org/unidal/maven/plugin/uml/EditMojo.java
@@ -5,11 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.zip.ZipInputStream;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -17,14 +17,14 @@ import org.apache.maven.project.MavenProject;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.unidal.helper.Files;
-import org.unidal.helper.Files.Policy;
 
 /**
- * Generate UML diagram from text files.
+ * Generate UML diagram from text.
  * 
  * @goal edit
  * @aggregator true
  * @requiresProject false
+ * @requiresDependencyResolution runtime
  * @author Frankie Wu
  */
 public class EditMojo extends AbstractMojo {
@@ -37,6 +37,8 @@ public class EditMojo extends AbstractMojo {
 	 */
 	private MavenProject m_project;
 
+	protected File m_warFile;
+
 	private void display(Server server) throws Exception {
 		int port = server.getConnectors()[0].getPort();
 		String uri = String.format("http://localhost:%s/uml", port);
@@ -46,56 +48,40 @@ public class EditMojo extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		String warRoot = getWarRoot();
-
 		try {
+			String warRoot = getWarRoot();
 			Server server = startServer(warRoot);
-			String mode = System.getProperty("mode", "local");
 
-			if ("local".equals(mode)) {
-				display(server);
-			}
-
+			display(server);
 			waitForAnyKey();
 		} catch (Exception e) {
-			throw new MojoExecutionException(
-					"Error when starting up server at " + warRoot + "!", e);
+			throw new MojoExecutionException("Error when starting up server! " + e, e);
 		}
 	}
 
-	private String getWarRoot() {
-		URL url = getClass().getResource("/webapp");
-		String protocol = url.getProtocol();
+	private String getWarRoot() throws Exception {
+		File warFile = null;
 
-		// extract webapp of jar into a temporary directory
-		if (protocol.equals("jar")) {
-			String path = url.getPath();
-
-			if (path.startsWith("file:")) {
-				File target = new File(m_project.getBasedir(), "target");
-				int pos = path.indexOf('!');
-				String jarPath = path.substring(5, pos);
-
-				try {
-					Files.forZip().copyDir(
-							new ZipInputStream(new FileInputStream(jarPath)),
-							target, new Policy() {
-								@Override
-								public boolean apply(String path) {
-									return path.startsWith("webapp/");
-								}
-							});
-
-					return new File(target, "webapp").getCanonicalPath();
-				} catch (Exception e) {
-					throw new RuntimeException(String.format(
-							"Error when extracting jar(%s) to %s!", jarPath,
-							target));
-				}
+		for (Artifact a : m_project.getDependencyArtifacts()) {
+			if ("war".equals(a.getType())) {
+				warFile = a.getFile();
+				break;
 			}
 		}
 
-		return url.getPath();
+		if (warFile == null) {
+			throw new RuntimeException("No war file found in local repository!");
+		}
+
+		File target = new File(m_project.getBasedir(), "target/webapp");
+
+		try {
+			Files.forZip().copyDir(new ZipInputStream(new FileInputStream(warFile)), target, null);
+
+			return target.getCanonicalPath();
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("Error when extracting war(%s) to %s!", warFile, target));
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -106,8 +92,7 @@ public class EditMojo extends AbstractMojo {
 		webapp.setContextPath("/");
 		webapp.setDescriptor(new File(warRoot, "WEB-INF/web.xml").getPath());
 		webapp.setResourceBase(warRoot);
-		webapp.getInitParams().put(
-				"org.mortbay.jetty.servlet.Default.dirAllowed", "false");
+		webapp.getInitParams().put("org.mortbay.jetty.servlet.Default.dirAllowed", "false");
 		server.setHandler(webapp);
 
 		server.start();
@@ -115,11 +100,9 @@ public class EditMojo extends AbstractMojo {
 	}
 
 	private void waitForAnyKey() throws IOException {
-		String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
-				.format(new Date());
+		String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
 
-		System.out.println(String.format(
-				"[%s] [INFO] Press any key to stop server ... ", timestamp));
+		System.out.println(String.format("[%s] [INFO] Press any key to stop server ... ", timestamp));
 		System.in.read();
 	}
 }
