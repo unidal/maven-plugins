6b24d24fa57994465a11fc1d746cd5a059d41d0d
Frankie Wu <qmwu2000@gmail.com>
2016-08-24 22:34:14 +0800

    formated

--- project-maven-plugin/src/main/java/org/unidal/maven/plugin/project/MigrateMojo.java
+++ project-maven-plugin/src/main/java/org/unidal/maven/plugin/project/MigrateMojo.java
@@ -21,272 +21,271 @@ import org.unidal.helper.Scanners.FileMatcher;
 import org.unidal.helper.Splitters;
 
 /**
- * Migrate all source files from one project to another project using a
- * different package base.If current project is a pom project, then all its
- * module project will be migrated as well.
+ * Migrate all source files from one project to another project using a different package base.If current project is a pom project,
+ * then all its module project will be migrated as well.
  * 
  * @goal migrate
  */
 public class MigrateMojo extends AbstractMojo {
-   /**
-    * Current project
-    * 
-    * @parameter expression="${project}"
-    * @required
-    * @readonly
-    */
-   private MavenProject project;
+	/**
+	 * Current project
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
 
-   /**
-    * @parameter expression="${sourcePackage}"
-    */
-   private String sourcePackage;
+	/**
+	 * @parameter expression="${sourcePackage}"
+	 */
+	private String sourcePackage;
 
-   /**
-    * @parameter expression="${targetPackage}"
-    */
-   private String targetPackage;
+	/**
+	 * @parameter expression="${targetPackage}"
+	 */
+	private String targetPackage;
 
-   /**
-    * @parameter expression="${targetDir}"
-    *            default-value="target/${project.artifactId}"
-    * @required
-    */
-   private String targetDir;
+	/**
+	 * @parameter expression="${targetDir}" default-value="target/${project.artifactId}"
+	 * @required
+	 */
+	private String targetDir;
 
-   /**
-    * @parameter expression="${verbose}" default-value="false"
-    * @required
-    */
-   private boolean verbose;
+	/**
+	 * @parameter expression="${verbose}" default-value="false"
+	 * @required
+	 */
+	private boolean verbose;
 
-   private String m_reversedSourcePackage;
+	private String m_reversedSourcePackage;
 
-   private String m_reversedTargetPackage;
+	private String m_reversedTargetPackage;
 
-   private int m_success;
+	private int m_success;
 
-   private int m_changed;
+	private int m_changed;
 
-   private int m_failure;
+	private int m_failure;
 
-   public void execute() throws MojoExecutionException, MojoFailureException {
-      sourcePackage = PropertyProviders.fromConsole().forString("sourcePackage", "Java source package:", sourcePackage, null);
-      targetPackage = PropertyProviders.fromConsole().forString("targetPackage", "Java target package:", targetPackage, null);
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		sourcePackage = PropertyProviders.fromConsole().forString("sourcePackage", "Java source package:", sourcePackage,
+		      null);
+		targetPackage = PropertyProviders.fromConsole().forString("targetPackage", "Java target package:", targetPackage,
+		      null);
 
-      m_reversedSourcePackage = reversePackage(sourcePackage);
-      m_reversedTargetPackage = reversePackage(targetPackage);
+		m_reversedSourcePackage = reversePackage(sourcePackage);
+		m_reversedTargetPackage = reversePackage(targetPackage);
 
-      try {
-         long start = System.currentTimeMillis();
-         File baseDir = project.getBasedir().getCanonicalFile();
-         File currentDir = new File(".").getCanonicalFile();
-         File targetBase = getTargetBaseDir(baseDir, currentDir);
-         final Set<String> sourceFolders = new HashSet<String>();
+		try {
+			long start = System.currentTimeMillis();
+			File baseDir = project.getBasedir().getCanonicalFile();
+			File currentDir = new File(".").getCanonicalFile();
+			File targetBase = getTargetBaseDir(baseDir, currentDir);
+			final Set<String> sourceFolders = new HashSet<String>();
 
-         m_success = 0;
-         m_changed = 0;
-         m_failure = 0;
+			m_success = 0;
+			m_changed = 0;
+			m_failure = 0;
 
-         for (String source : (List<String>) project.getCompileSourceRoots()) {
-            sourceFolders.add(source);
-         }
+			for (String source : (List<String>) project.getCompileSourceRoots()) {
+				sourceFolders.add(source);
+			}
 
-         for (String testSource : (List<String>) project.getTestCompileSourceRoots()) {
-            sourceFolders.add(testSource);
-         }
+			for (String testSource : (List<String>) project.getTestCompileSourceRoots()) {
+				sourceFolders.add(testSource);
+			}
 
-         for (Resource resource : (List<Resource>) project.getResources()) {
-            sourceFolders.add(resource.getDirectory());
-         }
+			for (Resource resource : (List<Resource>) project.getResources()) {
+				sourceFolders.add(resource.getDirectory());
+			}
 
-         for (Resource testResource : (List<Resource>) project.getTestResources()) {
-            sourceFolders.add(testResource.getDirectory());
-         }
+			for (Resource testResource : (List<Resource>) project.getTestResources()) {
+				sourceFolders.add(testResource.getDirectory());
+			}
 
-         if (new File(baseDir, "pom.xml").exists()) {
-            migrateFile(new File(baseDir, "pom.xml"), new File(targetBase, "pom.xml"));
-         }
+			if (new File(baseDir, "pom.xml").exists()) {
+				migrateFile(new File(baseDir, "pom.xml"), new File(targetBase, "pom.xml"));
+			}
 
-         if ("war".equals(project.getPackaging())) {
-            sourceFolders.add("src/main/webapp");
-         }
+			if ("war".equals(project.getPackaging())) {
+				sourceFolders.add("src/main/webapp");
+			}
 
-         Scanners.forDir().scan(baseDir, new FileMatcher() {
-            @Override
-            public Direction matches(File base, String path) {
-               if (path.equals("src") || path.startsWith("src/")) {
-                  List<String> parts = Splitters.by('/').split(path);
+			Scanners.forDir().scan(baseDir, new FileMatcher() {
+				@Override
+				public Direction matches(File base, String path) {
+					if (path.equals("src") || path.startsWith("src/")) {
+						List<String> parts = Splitters.by('/').split(path);
 
-                  if (parts.size() == 3) {
-                     sourceFolders.add(path);
-                     return Direction.MATCHED;
-                  } else {
-                     return Direction.DOWN;
-                  }
-               }
+						if (parts.size() == 3) {
+							sourceFolders.add(path);
+							return Direction.MATCHED;
+						} else {
+							return Direction.DOWN;
+						}
+					}
 
-               return Direction.NEXT;
-            }
-         });
+					return Direction.NEXT;
+				}
+			});
 
-         for (String sourceFolder : sourceFolders) {
-            migrateSource(baseDir, targetBase, sourceFolder);
-         }
+			for (String sourceFolder : sourceFolders) {
+				migrateSource(baseDir, targetBase, sourceFolder);
+			}
 
-         long timeUsed = System.currentTimeMillis() - start;
+			long timeUsed = System.currentTimeMillis() - start;
 
-         getLog().info(
-               String.format("%s files migrated in %s ms, with %s changed and %s failures.", m_success, timeUsed, m_changed,
-                     m_failure));
-      } catch (Exception e) {
-         String message = String.format("Error when migrating project[sourcePackage: %s, targetPackage: %s, targetDir: %s].",
-               sourcePackage, targetPackage, targetDir);
+			getLog().info(String.format("%s files migrated in %s ms, with %s changed and %s failures.", //
+			      m_success, timeUsed, m_changed, m_failure));
+		} catch (Exception e) {
+			String message = String.format("Error when migrating project[sourcePackage: %s, targetPackage: %s," //
+			      + " targetDir: %s].", sourcePackage, targetPackage, targetDir);
 
-         getLog().error(message);
-         throw new MojoFailureException(message, e);
-      }
-   }
+			getLog().error(message);
+			throw new MojoFailureException(message, e);
+		}
+	}
 
-   private File getTargetBaseDir(File baseDir, File currentDir) throws IOException {
-      File targetBase;
+	private File getTargetBaseDir(File baseDir, File currentDir) throws IOException {
+		File targetBase;
 
-      if (baseDir.equals(currentDir)) { // current project
-         if (targetDir.startsWith("/")) {
-            targetBase = new File(targetDir);
-         } else {
-            targetBase = new File(currentDir, targetDir);
-         }
-      } else {
-         String basePath = baseDir.getCanonicalPath();
-         String currentPath = currentDir.getCanonicalPath();
+		if (baseDir.equals(currentDir)) { // current project
+			if (targetDir.startsWith("/")) {
+				targetBase = new File(targetDir);
+			} else {
+				targetBase = new File(currentDir, targetDir);
+			}
+		} else {
+			String basePath = baseDir.getCanonicalPath();
+			String currentPath = currentDir.getCanonicalPath();
 
-         if (basePath.startsWith(currentPath)) { // sub-module
-            String relativePath = basePath.substring(currentPath.length());
+			if (basePath.startsWith(currentPath)) { // sub-module
+				String relativePath = basePath.substring(currentPath.length());
 
-            if (targetDir.startsWith("/")) {
-               targetBase = new File(targetDir, relativePath);
-            } else {
-               targetBase = new File(new File(currentDir, targetDir), relativePath);
-            }
-         } else {
-            throw new IllegalStateException(String.format("Invalid directory to run maven build! base: %s, current: %s.", basePath,
-                  currentPath));
-         }
-      }
+				if (targetDir.startsWith("/")) {
+					targetBase = new File(targetDir, relativePath);
+				} else {
+					targetBase = new File(new File(currentDir, targetDir), relativePath);
+				}
+			} else {
+				throw new IllegalStateException(String.format(
+				      "Invalid directory to run maven build! base: %s, current: %s.", basePath, currentPath));
+			}
+		}
 
-      return targetBase;
-   }
+		return targetBase;
+	}
 
-   protected void migrateDirectory(File source, File target, String sourcePath, String sourcePackageName) {
-      String targetPath;
+	protected void migrateDirectory(File source, File target, String sourcePath, String sourcePackageName) {
+		String targetPath;
 
-      if (sourcePackageName != null && sourcePackageName.startsWith(sourcePackage)) {
-         String targetPackageName = targetPackage + sourcePackageName.substring(sourcePackage.length());
+		if (sourcePackageName != null && sourcePackageName.startsWith(sourcePackage)) {
+			String targetPackageName = targetPackage + sourcePackageName.substring(sourcePackage.length());
 
-         targetPath = targetPackageName.replace('.', '/');
-      } else {
-         targetPath = sourcePath;
-      }
+			targetPath = targetPackageName.replace('.', '/');
+		} else {
+			targetPath = sourcePath;
+		}
 
-      File base = sourcePath == null ? source : new File(source, sourcePath);
-      String[] names = base.list();
+		File base = sourcePath == null ? source : new File(source, sourcePath);
+		String[] names = base.list();
 
-      if (names != null) {
-         for (String name : names) {
-            File file = new File(base, name);
+		if (names != null) {
+			for (String name : names) {
+				File file = new File(base, name);
 
-            if (file.isDirectory()) {
-               String newPath = sourcePath == null ? name : sourcePath + "/" + name;
-               String newPackageName = sourcePackageName == null ? name : sourcePackageName + "." + name;
+				if (file.isDirectory()) {
+					String newPath = sourcePath == null ? name : sourcePath + "/" + name;
+					String newPackageName = sourcePackageName == null ? name : sourcePackageName + "." + name;
 
-               migrateDirectory(source, target, newPath, newPackageName);
-            } else if (file.isFile()) {
-               if (targetPath == null) {
-                  migrateFile(file, new File(target, name));
-               } else {
-                  migrateFile(file, new File(target, targetPath + "/" + name));
-               }
-            }
-         }
-      }
-   }
+					migrateDirectory(source, target, newPath, newPackageName);
+				} else if (file.isFile()) {
+					if (targetPath == null) {
+						migrateFile(file, new File(target, name));
+					} else {
+						migrateFile(file, new File(target, targetPath + "/" + name));
+					}
+				}
+			}
+		}
+	}
 
-   protected void migrateFile(File source, File target) {
-      try {
-         String encoding = "utf-8";
-         String original = Files.forIO().readFrom(source, encoding);
-         String migrated = replaceAll(original, sourcePackage, targetPackage);
+	protected void migrateFile(File source, File target) {
+		try {
+			String encoding = "utf-8";
+			String original = Files.forIO().readFrom(source, encoding);
+			String migrated = replaceAll(original, sourcePackage, targetPackage);
 
-         // for tld uri, cookie domain name etc.
-         migrated = replaceAll(migrated, m_reversedSourcePackage, m_reversedTargetPackage);
+			// for tld uri, cookie domain name etc.
+			migrated = replaceAll(migrated, m_reversedSourcePackage, m_reversedTargetPackage);
 
-         Files.forIO().writeTo(target, migrated, encoding);
-         m_success++;
+			Files.forIO().writeTo(target, migrated, encoding);
+			m_success++;
 
-         boolean changed = !original.equals(migrated);
+			boolean changed = !original.equals(migrated);
 
-         if (changed) {
-            m_changed++;
-         }
+			if (changed) {
+				m_changed++;
+			}
 
-         if (verbose) {
-            if (changed) {
-               getLog().info(String.format("File(%s) migrated, content length is %s.", target, target.length()));
-            } else {
-               getLog().info(String.format("File(%s) copied, content length is %s.", target, target.length()));
-            }
-         }
-      } catch (Exception e) {
-         getLog().warn(String.format("Error when migrating file(%s)!", source), e);
-         m_failure++;
-      }
-   }
+			if (verbose) {
+				if (changed) {
+					getLog().info(String.format("File(%s) migrated, content length is %s.", target, target.length()));
+				} else {
+					getLog().info(String.format("File(%s) copied, content length is %s.", target, target.length()));
+				}
+			}
+		} catch (Exception e) {
+			getLog().warn(String.format("Error when migrating file(%s)!", source), e);
+			m_failure++;
+		}
+	}
 
-   protected void migrateSource(File sourceBase, File targetBase, String path) {
-      String basePath = sourceBase.getPath();
-      String relativePath;
+	protected void migrateSource(File sourceBase, File targetBase, String path) {
+		String basePath = sourceBase.getPath();
+		String relativePath;
 
-      if (path.startsWith(basePath)) {
-         relativePath = path.substring(basePath.length());
-      } else {
-         relativePath = path;
-      }
+		if (path.startsWith(basePath)) {
+			relativePath = path.substring(basePath.length());
+		} else {
+			relativePath = path;
+		}
 
-      File source = new File(sourceBase, relativePath);
-      File target = new File(targetBase, relativePath);
+		File source = new File(sourceBase, relativePath);
+		File target = new File(targetBase, relativePath);
 
-      migrateDirectory(source, target, null, null);
-   }
+		migrateDirectory(source, target, null, null);
+	}
 
-   protected String replaceAll(String source, String fromToken, String toToken) {
-      int toLength = toToken.length();
-      int fromLength = fromToken.length();
-      StringBuilder sb = new StringBuilder(source.length() + (toLength < fromLength ? 0 : toLength - fromLength) * 30);
-      int offset = 0;
-      int index = source.indexOf(fromToken, offset);
+	protected String replaceAll(String source, String fromToken, String toToken) {
+		int toLength = toToken.length();
+		int fromLength = fromToken.length();
+		StringBuilder sb = new StringBuilder(source.length() + (toLength < fromLength ? 0 : toLength - fromLength) * 30);
+		int offset = 0;
+		int index = source.indexOf(fromToken, offset);
 
-      while (true) {
-         if (index == -1) {
-            sb.append(source.substring(offset));
-            break;
-         } else {
-            sb.append(source.substring(offset, index));
-            sb.append(toToken);
+		while (true) {
+			if (index == -1) {
+				sb.append(source.substring(offset));
+				break;
+			} else {
+				sb.append(source.substring(offset, index));
+				sb.append(toToken);
 
-            offset = index + fromLength;
-            index = source.indexOf(fromToken, offset);
-         }
-      }
+				offset = index + fromLength;
+				index = source.indexOf(fromToken, offset);
+			}
+		}
 
-      return sb.toString();
-   }
+		return sb.toString();
+	}
 
-   protected String reversePackage(String packageName) {
-      List<String> parts = Splitters.by('.').split(packageName);
+	protected String reversePackage(String packageName) {
+		List<String> parts = Splitters.by('.').split(packageName);
 
-      Collections.reverse(parts);
+		Collections.reverse(parts);
 
-      return Joiners.by('.').join(parts);
-   }
+		return Joiners.by('.').join(parts);
+	}
 }
