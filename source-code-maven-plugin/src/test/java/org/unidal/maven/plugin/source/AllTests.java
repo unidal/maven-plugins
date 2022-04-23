c091c18bc92bf72ec1b337108a691a61a497032e
Frankie Wu <qmwu2000@gmail.com>
2022-04-23 15:38:08 +0800

    fix test failures

--- source-code-maven-plugin/src/test/java/org/unidal/maven/plugin/source/AllTests.java
+++ source-code-maven-plugin/src/test/java/org/unidal/maven/plugin/source/AllTests.java
@@ -1,16 +1,14 @@
-package org.unidal.maven.plugin.project;
+package org.unidal.maven.plugin.source;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.unidal.maven.plugin.project.rule.RuleEngineTest;
+import org.unidal.maven.plugin.source.pipeline.SourcePipelineTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 
-//MigrateMojoTest.class,
-
-RuleEngineTest.class
+      SourcePipelineTest.class,
 
 })
 public class AllTests {
